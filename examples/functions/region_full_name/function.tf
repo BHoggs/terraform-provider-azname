c2d30f7799805dfe3c1e72db21b8e61af7a43ae1
Brett Hoggins <brett.hoggs@gmail.com>
2025-11-26 23:28:29 +1300

    Remove silly example

--- examples/functions/region_full_name/function.tf
+++ examples/functions/region_full_name/function.tf
@@ -6,10 +6,6 @@ terraform {
   }
 }
 
-provider "azname" {
-  environment = "prod"
-}
-
 # Convert Azure region CLI name to full display name
 # Useful for displaying user-friendly region names
 output "full_name_from_cli" {
@@ -20,16 +16,4 @@ output "full_name_from_cli" {
 output "full_name_from_short" {
   value = provider::azname::region_full_name("wus2")
   # Returns: "West US 2"
-}
-
-# Common use case: displaying region names in outputs or UI
-resource "azname_name" "example" {
-  name          = "myapp"
-  resource_type = "azurerm_resource_group"
-  location      = "westus2"
-}
-
-output "friendly_location" {
-  description = "User-friendly region name for documentation"
-  value       = provider::azname::region_full_name("westus2")
 }
